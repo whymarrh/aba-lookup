fc837bbe05a82e045ce051eb5f1637630f6d0aaf
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2013-10-13 14:35:34 -0230

    Added docs and test for Geocoding API.

--- module/Maps/src/Maps/Api.php
+++ module/Maps/src/Maps/Api.php
@@ -6,10 +6,21 @@ use
 	Maps\Geocoding\Api as GeocodingApi
 ;
 
+/**
+ * Wrapper API for the Google Maps API Web Services
+ */
 class Api
 {
-	public function getLatLng($address)
+	/**
+	 * Returns a LatLng object representing address
+	 *
+	 * @param string $address The address to geocode.
+	 * @param string $region The ccTLD of a region to bias towards. Defaults is Canada.
+	 * @return LatLng
+	 * @throws InvalidArgumentException
+	 */
+	public function getLatLng($address, $region = 'ca')
 	{
-		return GeocodingApi::getLatLng($address);
+		return GeocodingApi::getLatLng($address, $region);
 	}
 }

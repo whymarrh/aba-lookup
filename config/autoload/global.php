be27ab89eef6e8fc9c9443bc33f2eac6e2127339
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-11-16 14:17:58 -0330

    Add Zend Db Adapter

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-	'dsn' => 'sqlite::memory:',
+	'db' => [
+		'driver' => 'Pdo',
+		'dsn' => sprintf('%s:%s', 'sqlite', realpath(sprintf('%s/%s', __DIR__, '../../database/db.sqlite3'))),
+	],
+	'service_manager' => [
+		'factories' => [
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+		],
+	],
 ];

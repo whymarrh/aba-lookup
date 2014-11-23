f61bc8d3377a914fb298eb80149b6e3af23a12d9
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-11-23 01:55:21 -0330

    Add service config for Lookup module

--- module/Lookup/config/module.config.php
+++ module/Lookup/config/module.config.php
@@ -1,4 +1,14 @@
 <?php
 
-return [
-];
+return array(
+	'service_manager' => array(
+		'factories' => array(
+			'Lookup\Api\Schedule' => 'Lookup\Api\ServiceFactory\ScheduleServiceFactory',
+			'Lookup\Api\UserAccount' => 'Lookup\Api\ServiceFactory\UserAccountServiceFactory',
+			'Lookup\Db\Location' => 'Lookup\Db\ServiceFactory\LocationServiceFactory',
+			'Lookup\Db\UserAccount' => 'Lookup\Db\ServiceFactory\UserAccountServiceFactory',
+			'Lookup\Db\UserDisplayName' => 'Lookup\Db\ServiceFactory\UserDisplayNameServiceFactory',
+			'Lookup\Db\UserType' => 'Lookup\Db\ServiceFactory\UserTypeServiceFactory',
+		),
+	),
+);

35fab91939a2663684dad806f51983b949875a91
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-11-23 01:59:10 -0330

    Add test data

---
+++ data/test-data.sql
@@ -0,0 +1,21 @@
+-- User types
+INSERT INTO user_type VALUES(
+	1, "parent"
+);
+INSERT INTO user_type VALUES(
+	2, "therapist"
+);
+
+-- Locations
+INSERT INTO location VALUES(
+	1, "St. John's", "A1A 1A1"
+);
+INSERT INTO location VALUES(
+	2, "Port Morien", "B1B 1B1"
+);
+INSERT INTO location VALUES(
+	3, "Charlottetown", "C1C 1C1"
+);
+INSERT INTO location VALUES(
+	4, "Moncton", "E1E 1E1"
+);

50aaf6f4451cadb79e7259fa2a208425d3f6164b
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2014-11-30 23:55:33 -0330

    Cleanup bootstrap file for module tests

--- module/AbaLookup/test/Bootstrap.php
+++ module/AbaLookup/test/Bootstrap.php
@@ -2,33 +2,16 @@
 
 namespace AbaLookupTest;
 
-use
-	RuntimeException,
-	Zend\Loader\AutoloaderFactory,
-	Zend\Mvc\Service\ServiceManagerConfig,
-	Zend\ServiceManager\ServiceManager
-;
+use RuntimeException;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
 
 error_reporting(E_ALL | E_STRICT);
 chdir(__DIR__);
 
-/**
- * PHPUnit bootstrap file
- */
 class Bootstrap
 {
-	/**
-	 * @var Zend\ServiceManager\ServiceManager
-	 */
-	protected static $serviceManager;
-
-	/**
-	 * Return the full path to the given folder
-	 *
-	 * Return the full path to the given
-	 * folder provided it lives as a parent
-	 * to the current folder.
-	 */
 	protected static function findParentPath($path)
 	{
 		$dir = __DIR__;
@@ -43,72 +26,40 @@ class Bootstrap
 		return $dir . '/' . $path;
 	}
 
-	/**
-	 * Return the the ServiceManager
-	 */
-	public static function getServiceManager()
-	{
-		return static::$serviceManager;
-	}
-
-	/**
-	 * Initialise the bootstrap class
-	 */
 	public static function init()
 	{
-		// The path to the module directory
-		$zf2ModulePaths = [dirname(dirname(__DIR__))];
-		// Find the vendor directory
-		if (($path = static::findParentPath('vendor'))) {
-			$zf2ModulePaths[] = $path;
-		}
-		// Check for another module directory
-		if (($path = static::findParentPath('module')) !== $zf2ModulePaths[0]) {
-			$zf2ModulePaths[] = $path;
-		}
-
 		static::initAutoloader();
 
-		// Use ModuleManager to load this module and its dependencies
-		$config = [
-			'module_listener_options' => [
-				'module_paths' => $zf2ModulePaths,
-			],
-			'modules' => [
+		$config = array(
+			'module_listener_options' => array(
+				'config_glob_paths' => array(
+					'../../../config/autoload/{,*.}{global,local}.php',
+				),
+				'module_paths' => array(
+					static::findParentPath('vendor'),
+					static::findParentPath('module'),
+				),
+			),
+			'modules' => array(
 				'AbaLookup',
-			],
-		];
+			),
+		);
 		$serviceManager = new ServiceManager(new ServiceManagerConfig());
 		$serviceManager->setService('ApplicationConfig', $config);
 		$serviceManager->get('ModuleManager')->loadModules();
-		static::$serviceManager = $serviceManager;
 	}
 
-	/**
-	 * Load the autoloader file
-	 */
 	protected static function initAutoloader()
 	{
-		// Autoload file
-		$autoloadFile = realpath(sprintf(
-			'%s/autoload.php',
-			static::findParentPath('vendor')
-		));
-		// Composer autoloading
-		if (file_exists($autoloadFile)) {
-			$loader = include $autoloadFile;
-		}
-		if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-			throw new RuntimeException('Unable to load ZF2. Run "composer install".');
-		}
-		AutoloaderFactory::factory([
-			'Zend\Loader\StandardAutoloader' => [
+		include static::findParentPath('vendor') . '/../autoloader.php';
+		AutoloaderFactory::factory(array(
+			'Zend\Loader\StandardAutoloader' => array(
 				'autoregister_zf' => TRUE,
-				'namespaces'      => [
+				'namespaces' => array(
 					__NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-				],
-			],
-		]);
+				),
+			),
+		));
 	}
 }
 
--- module/Lookup/test/Bootstrap.php
+++ module/Lookup/test/Bootstrap.php
@@ -12,18 +12,6 @@ chdir(__DIR__);
 
 class Bootstrap
 {
-	/**
-	 * @var Zend\ServiceManager\ServiceManager
-	 */
-	protected static $serviceManager;
-
-	/**
-	 * Return the full path to the given folder
-	 *
-	 * Return the full path to the given
-	 * folder provided it lives as a parent
-	 * to the current folder.
-	 */
 	protected static function findParentPath($path)
 	{
 		$dir = __DIR__;
@@ -38,72 +26,40 @@ class Bootstrap
 		return $dir . '/' . $path;
 	}
 
-	/**
-	 * Return the the ServiceManager
-	 */
-	public static function getServiceManager()
-	{
-		return static::$serviceManager;
-	}
-
-	/**
-	 * Initialise the bootstrap class
-	 */
 	public static function init()
 	{
-		// The path to the module directory
-		$zf2ModulePaths = [dirname(dirname(__DIR__))];
-		// Find the vendor directory
-		if (($path = static::findParentPath('vendor'))) {
-			$zf2ModulePaths[] = $path;
-		}
-		// Check for another module directory
-		if (($path = static::findParentPath('module')) !== $zf2ModulePaths[0]) {
-			$zf2ModulePaths[] = $path;
-		}
-
 		static::initAutoloader();
 
-		// Use ModuleManager to load this module and its dependencies
-		$config = [
-			'module_listener_options' => [
-				'module_paths' => $zf2ModulePaths,
-			],
-			'modules' => [
+		$config = array(
+			'module_listener_options' => array(
+				'config_glob_paths' => array(
+					'../../../config/autoload/{,*.}{global,local}.php',
+				),
+				'module_paths' => array(
+					static::findParentPath('vendor'),
+					static::findParentPath('module'),
+				),
+			),
+			'modules' => array(
 				'Lookup',
-			],
-		];
+			),
+		);
 		$serviceManager = new ServiceManager(new ServiceManagerConfig());
 		$serviceManager->setService('ApplicationConfig', $config);
 		$serviceManager->get('ModuleManager')->loadModules();
-		static::$serviceManager = $serviceManager;
 	}
 
-	/**
-	 * Load the autoloader file
-	 */
 	protected static function initAutoloader()
 	{
-		// Autoload file
-		$autoloadFile = realpath(sprintf(
-			'%s/autoload.php',
-			static::findParentPath('vendor')
-		));
-		// Composer autoloading
-		if (file_exists($autoloadFile)) {
-			$loader = include $autoloadFile;
-		}
-		if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-			throw new RuntimeException('Unable to load ZF2. Run "composer install".');
-		}
-		AutoloaderFactory::factory([
-			'Zend\Loader\StandardAutoloader' => [
+		include static::findParentPath('vendor') . '/../autoloader.php';
+		AutoloaderFactory::factory(array(
+			'Zend\Loader\StandardAutoloader' => array(
 				'autoregister_zf' => TRUE,
-				'namespaces'      => [
+				'namespaces' => array(
 					__NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
-				],
-			],
-		]);
+				),
+			),
+		));
 	}
 }
 
